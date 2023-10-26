05b66430df5b1c63e13c479bab3641c18fa50e21
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-26 16:03:57 +0500

    mfe courses

--- src/components/courses/ShowCourses.jsx
+++ src/components/courses/ShowCourses.jsx
@@ -1,28 +1,96 @@
-import { Container } from '@edx/paragon';
 
-export default function ShowCourses() {
-  fetch("http://apps.local.overhang.io:8000/api/cookiecutter_courses/v1/list/", { mode: "no-cors" })
-    .then(response => {
-      if (response.ok) {
-        return response.json(); // Parse the response as JSON
-      }
-      throw new Error(`Request failed with status: ${response.status}`);
-    })
-    .then(data => {
-      console.log("Response data:", data); // Log the parsed response data
-    })
-    .catch(error => {
-      console.log("Error:", error)
-      console.error("Error:", error);
+import React, { useEffect, useState } from "react";
+
+import { Container, Spinner } from "@edx/paragon";
+import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+import useHttp from "../../hooks/use-https";
+
+const ShowCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const { sendRequest, isLoading, error } = useHttp();
+
+  useEffect(() => {
+    sendRequest({ url: "api/cookiecutter_courses/v1/list/" }, (data) => {
+      setCourses(data.results);
     });
+  }, [sendRequest]);
 
   return (
-    <Container className="py-5">
-      <h1>Example courses</h1>
-      <p>Welcome !</p>
-    </Container>
-  )
-}
+    <main>
+      <Container className="py-5">
+        <Grid
+          container
+          spacing={{ xs: 2, md: 3 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+        >
+          {isLoading ? (
+            <Grid item xs={2} sm={4} md={4}>
+              <Spinner
+                animation="border"
+                variant="info"
+                className="mr-3"
+                screenReaderText="loading"
+              />
+            </Grid>
+          ) : error ? (
+            <Grid item xs={2} sm={4} md={4}>
+              <h2>{error}</h2>
+            </Grid>
+          ) : (
+            courses?.map((item) => (
+              <Grid item xs={2} sm={4} md={4} key={item.id}>
+                <Card sx={{ maxWidth: 300 }}>
+                  <CardMedia
+                    sx={{ height: 200 }}
+                    image={item.media.image.raw}
+                    title={item.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                      {item.name}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))
+          )}
+        </Grid>
+      </Container>
+    </main>
+  );
+};
+
+export default ShowCourses;
+// import { Container } from '@edx/paragon';
+
+// export default function ShowCourses() {
+//   fetch("http://apps.local.overhang.io:8000/api/cookiecutter_courses/v1/list/", { mode: "no-cors" })
+//     .then(response => {
+//       if (response.ok) {
+//         return response.json(); // Parse the response as JSON
+//       }
+//       throw new Error(`Request failed with status: ${response.status}`);
+//     })
+//     .then(data => {
+//       console.log("Response data:", data); // Log the parsed response data
+//     })
+//     .catch(error => {
+//       console.log("Error:", error)
+//       console.error("Error:", error);
+//     });
+
+//   return (
+//     <Container className="py-5">
+//       <h1>Example courses</h1>
+//       <p>Welcome !</p>
+//     </Container>
+//   )
+// }
 
 
 
