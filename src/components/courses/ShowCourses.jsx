935d08ac6718adda200df8295aa977093f93bc2a
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-26 18:47:44 +0500

    mfe courses

--- src/components/courses/ShowCourses.jsx
+++ src/components/courses/ShowCourses.jsx
@@ -5,7 +5,7 @@ const ShowCourses = () => {
   const [courses, setCourses] = useState([]);
 
   const fetchData = () => {
-    fetch('api/cookiecutter_courses/v1/list/')
+    fetch('http://local.overhang.io:8000/api/cookiecutter_courses/v1/list/')
       .then((response) => {
         if (!response.ok) {
           throw new Error(`Network response was not ok (status ${response.status})`);
@@ -25,7 +25,7 @@ const ShowCourses = () => {
 
   return (
     <Container>
-      <h1>Example courses</h1>
+      <h1>courses</h1>
       <p>Welcome to Edx courses!</p>
       <ul>
         {courses.map((course) => (
