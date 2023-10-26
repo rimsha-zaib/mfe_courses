fe0292517ac3107fa4b84ffe954c1a0e514f1008
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-27 00:58:43 +0500

    mfe courses

--- src/components/courses/ShowCourses.jsx
+++ src/components/courses/ShowCourses.jsx
@@ -1,15 +1,34 @@
-import { Container } from "@edx/paragon";
-import { getConfig } from '@edx/frontend-platform';
-
-const ShowCourses = async () => { 
-  const lmsUrl = getConfig().LMS_BASE_URL;
-  console.log("lmsurl",lmsUrl);
+import React, { useEffect, useState } from 'react';
+import { Container } from '@edx/paragon';
+const ShowCourses = () => {
+  const [courses, setCourses] = useState([]);
+  const fetchData = () => {
+    fetch('http://local.overhang.io:8000/api/cookiecutter_courses/v1/list/')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`Network response was not ok (status ${response.status})`);
+        }
+        return response.json();
+      })
+      .then((data) => setCourses(data))
+      .catch((error) => {
+        console.log('Error fetching data:', error);
+      });
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+  console.log('data', courses);
   return (
     <Container>
-       <h1>courses</h1>
-       <p>Welcome to Edx courses!</p>
+      <h1>courses</h1>
+      <p>Welcome to Edx courses!</p>
+      <ul>
+        {courses.map((course) => (
+          <li key={course.id}>{course.name}</li>
+        ))}
+      </ul>
     </Container>
   );
 };
-
 export default ShowCourses;
