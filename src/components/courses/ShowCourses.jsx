7689637dc15ed338d029603286cf224d97b143e6
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-27 13:13:32 +0500

    mfe courses

--- src/components/courses/ShowCourses.jsx
+++ src/components/courses/ShowCourses.jsx
@@ -26,8 +26,16 @@ const ShowCourses = () => {
       <h1>courses</h1>
       <p>Welcome to Edx courses!</p>
       <ul>
-        {courses.map((course) => (
-          <li key={course.id}>{course.name}</li>
+      {courses.map((course) => (
+          <li key={course.id}>
+            <strong>{course.name}</strong>
+            <p>Course ID: {course.course_id}</p>
+            <p>Start Date: {course.start_display}</p>
+            <p>Organization: {course.org}</p>
+            {course.media && course.media.image && (
+              <img src={course.media.image.raw} alt={course.name} />
+            )}
+          </li>
         ))}
       </ul>
     </Container>
