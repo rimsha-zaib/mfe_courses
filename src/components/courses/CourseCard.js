b60279df8942450d04941fb735a02b50f8268be2
“Rimsha <rimsha.zaib@arbisoft.com>
2023-10-27 15:13:05 +0500

    mfe courses

--- src/components/courses/CourseCard.js
+++ src/components/courses/CourseCard.js
@@ -1,99 +1,20 @@
-// import Card from '@mui/material/Card';
-// import CardContent from '@mui/material/CardContent';
-// import CardMedia from '@mui/material/CardMedia';
-// import Typography from '@mui/material/Typography';
-// import { Button, CardActionArea, CardActions } from '@mui/material';
-// import React from 'react';
-// import Alert from '@mui/material/Alert';
-// import { useEffect } from 'react';
+import React from 'react';
+import { Card, Image } from '@edx/paragon';
 
+const CourseCard = ({ course }) => (
+  <Card>
+    <Card.Image>
+      {course.media && course.media.image && (
+        <Image src={course.media.image.raw} alt={course.name} />
+      )}
+    </Card.Image>
+    <Card.Body>
+      <Card.Title>{course.name}</Card.Title>
+      <Card.Text>Course ID: {course.course_id}</Card.Text>
+      <Card.Text>Start Date: {course.start_display}</Card.Text>
+      <Card.Text>Organization: {course.org}</Card.Text>
+    </Card.Body>
+  </Card>
+);
 
-// const CourseCard = (props) =>  {
-//   const dispatch = useDispatch();
-//   const showAlert = useSelector((state) => state.course.showAlert);
-//   const showDeleteAlert = useSelector((state) => state.teacher.showAlert);
-//   console.log("deleteslert",showDeleteAlert)
-//   const userId = localStorage.getItem('userId');
-//   const courseId = { course_id: props.id};
-
-//   useEffect(() => {
-//     if (showAlert) {
-//       const timer = setTimeout(() => {
-//         dispatch(hideDeleteAlert())
-//         dispatch(hideAlert())
-//       }, 2000);
-//       return () => {
-//         clearTimeout(timer);
-//       };
-//     }
-//   }, [showAlert,showDeleteAlert]);
-
-// const enrollHandler = () => {
-//   dispatch(enrollCourse(courseId,userId)); 
-// };
-// const unenrollHandler = () => {
-//   dispatch(unenrollCourse(courseId,userId));
-// };
-// const deleteHandler = () => {
-//   const course_id= props.id
-//   console.log("course id = ",course_id)
-//   dispatch(deleteCourse(userId,courseId))
-// };
-//   return (
-//     <div>
-//     {showAlert && (
-//       <Alert variant="filled" severity="success" 
-//       sx={{ position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)' ,width: '100%',}}>
-//         {showAlert}
-//       </Alert>
-//     )}
-//     {showDeleteAlert && (
-//       <Alert variant="filled" severity="success" 
-//       sx={{ position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)' ,width: '100%',}}>
-//         {showDeleteAlert}
-//       </Alert>
-//     )}
-//     <Card sx={{ maxWidth: 345 }}>
-//       <CardActionArea>
-//         <CardMedia
-//           component="img"
-//           height="140"
-//           image={props.course_img}
-//           alt="green iguana"
-//         />
-//         <CardContent>
-//           <Typography gutterBottom variant="h5" component="div">
-//             {props.title}
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//            {props.description}
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//           <b>Duration:</b>{props.duration}
-//           </Typography>
-//           <Typography variant="body2" color="text.secondary">
-//            <b>Teacher:</b> {props.teacher_name}
-//           </Typography>
-//         </CardContent>
-//       </CardActionArea>
-//       <CardActions>
-//       {props.button === 'enroll' ? (
-//           <Button size="small" color="primary" onClick={enrollHandler}>
-//             Enroll
-//           </Button>
-//         ) : props.button === 'unenroll' ? (
-//           <Button size="small" color="primary" onClick={unenrollHandler}>
-//             UNEnroll
-//           </Button>
-//         ) : props.button === 'delete' ? (
-//           <Button size="small" color="primary" onClick={deleteHandler}>
-//             Delete
-//           </Button>
-//         ) : null}
-//       </CardActions>
-//     </Card>
-//   </div>
-//   );
-// }
-
-// export default CourseCard;
+export default CourseCard;
--- src/components/courses/ShowCourses.jsx
+++ src/components/courses/ShowCourses.jsx
@@ -1,26 +1,51 @@
+// import React, { useEffect, useState } from 'react';
+// import { Container } from '@edx/paragon';
+// import fetchCoursesApi from './data/fetchCourses';
+// const ShowCourses = () => {
+//   const courses = fetchCoursesApi();
+//   return (
+//     <Container>
+//       <h1>edX courses</h1>
+//       <p>Welcome to Edx courses!</p>
+//       <ul>
+//       {courses.map((course) => (
+//           <li key={course.id}>
+//             <strong>{course.name}</strong>
+//             <p>Course ID: {course.course_id}</p>
+//             <p>Start Date: {course.start_display}</p>
+//             <p>Organization: {course.org}</p>
+//             {course.media && course.media.image && (
+//               <img src={course.media.image.raw} alt={course.name} />
+//             )}
+//           </li>
+//         ))}
+//       </ul>
+//     </Container>
+//   );
+// };
+// export default ShowCourses;
+
 import React, { useEffect, useState } from 'react';
-import { Container } from '@edx/paragon';
+import { Container, Row, Col } from '@edx/paragon';
 import fetchCoursesApi from './data/fetchCourses';
+import CourseCard from './CourseCard';
+
 const ShowCourses = () => {
   const courses = fetchCoursesApi();
+
   return (
     <Container>
-      <h1>courses</h1>
+      <h1>edX courses</h1>
       <p>Welcome to Edx courses!</p>
-      <ul>
-      {courses.map((course) => (
-          <li key={course.id}>
-            <strong>{course.name}</strong>
-            <p>Course ID: {course.course_id}</p>
-            <p>Start Date: {course.start_display}</p>
-            <p>Organization: {course.org}</p>
-            {course.media && course.media.image && (
-              <img src={course.media.image.raw} alt={course.name} />
-            )}
-          </li>
+      <Row>
+        {courses.map((course) => (
+          <Col key={course.id} sm={12} md={6} lg={4}>
+            <CourseCard course={course} /> {/* Render each course as a card */}
+          </Col>
         ))}
-      </ul>
+      </Row>
     </Container>
   );
 };
+
 export default ShowCourses;
